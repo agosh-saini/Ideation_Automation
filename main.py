4c349f01f9bfec67e8e3e5041652aab70e5a7354
Agosh Saini <33809549+agosh-saini@users.noreply.github.com>
2023-10-11 23:44:41 -0400

    modified UI for more control

--- main.py
+++ main.py
@@ -22,12 +22,14 @@ if __name__ == '__main__':
               [sg.Text('Blank Lines'), sg.InputText(key='blank')],
               [sg.Text('List Columns'), sg.InputText(key='column')],
               [sg.Text('Smooth Value'), sg.InputText(key='smooth')],
-              [sg.Text('Graph'), sg.Text()],
-              [sg.Checkbox('Yes', default=True, key='graph')],
+              [sg.Checkbox('Entire Bound', default=True, key='entire_bound')],
+              [sg.Text('Lower Bound'), sg.InputText(key='lower_bound')],
+              [sg.Text('Upper Bound'), sg.InputText(key='upper_bound')],
+              [sg.Checkbox('Get Graphs', default=True, key='graph')],
               [sg.Text('Threshold'), sg.InputText(key='thres')],
               [sg.Text('Min Height'), sg.InputText(key='height')],
-              [sg.Text('Check for Peak, Leave Unchecked for Valley'), sg.Text()],
-              [sg.Checkbox('Get Peaks', default=False, key='peak')],
+              [sg.Text('Check for Peak, Unchecked for Valley'), sg.Text()],
+              [sg.Checkbox('Check for Peak, Uncheck for Valley', default=False, key='peak')],
               [sg.Button('Ok'), sg.Button('Cancel')]]
 
     # Create the Window
@@ -36,9 +38,10 @@ if __name__ == '__main__':
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = window.read()
-
+        if event == "Ok":
+            continue
         # if user closes window or clicks cancel or clicks ok -> exit
-        if event == sg.WIN_CLOSED or event == 'Cancel' or event == "Ok":
+        if event == sg.WIN_CLOSED or event == 'Cancel':
             break
     # close the window
     window.close()
@@ -54,6 +57,12 @@ if __name__ == '__main__':
         direction = -1
     else:
         direction = 1
+    # check to see if the person wants entire range or not
+    if values['entire_bound'] is True:
+        bound = None
+    else:
+        bound = [float(values["lower_bound"]), float(values("upper_bound"))]
+
 
     # repeat for all the files we have
     for file in files:
@@ -77,7 +86,8 @@ if __name__ == '__main__':
                       smooth=float(values['smooth']), graph=values['graph'],
                       threshold=float(values['thres']),
                       min_height=float(values['height']),
-                      direction=direction)
+                      direction=direction,
+                      bounds=bound)
 
     # -------- SCRIPT END ------------- #
 
